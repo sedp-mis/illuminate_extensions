e537b58f970e3de4d2e0b706be0d91feb7a6316b
arjon_jason_castro <ajcastro29@gmail.com>
2016-06-09 22:00:24 +0800

    Add some helper files. Add Makeable lib package

---
+++ src/Makeable/MakeableTrait.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace SedpMis\Lib\Makeable;
+
+trait MakeableTrait {
+
+    /**
+     * Instantiate or make new instance via static method,  can passed any number of parameters.
+     *
+     * @return static
+     */
+    public static function make()
+    {
+        return (new \ReflectionClass(get_called_class()))->newInstanceArgs(func_get_args());
+    }
+
+}
