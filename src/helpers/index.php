16fb8c1833bc8e08f7931866fec1d7e50586545d
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-13 20:46:00 +0800

    Simplify

--- src/helpers/index.php
+++ src/helpers/index.php
@@ -1,12 +1,10 @@
 <?php
 
-define('HELPERS_DIR', __DIR__);
-
-$helperFiles = scandir(HELPERS_DIR);
+$helperFiles = scandir(__DIR__);
 
 foreach ($helperFiles as $file) {
     // Require .php files only
     if (substr($file, -4, 4) == '.php') {
-        require_once HELPERS_DIR.'/'.$file;
+        require_once __DIR__.'/'.$file;
     }
 }
